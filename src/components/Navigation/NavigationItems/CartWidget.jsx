cd86750b254b1c700053c6753d820db574c3469b
Carlos Menéndez <menendezcarlos27@gmail.com>
2022-05-13 09:20:18 -0300

    Styles

--- src/components/Navigation/NavigationItems/CartWidget.jsx
+++ src/components/Navigation/NavigationItems/CartWidget.jsx
@@ -2,13 +2,13 @@ import './NavigationItems.css';
 
 const cartWidget = () => {
     return (
-        <div className="Cart">
-            <a href="#" className="btn-floating white">
+        <div className="Cart hide-on-med-and-down">
+            <button href="#" className="btn-floating white">
                 <i className="material-icons blue-text">shopping_cart</i>
-            </a>
-                <span className="CartBadge">
-                    2
-                </span>
+            </button>
+            <span className="Badge center">
+                2
+            </span>
         </div>
     )
 }
